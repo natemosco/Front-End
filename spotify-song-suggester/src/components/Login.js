668f28b7cb3914ae4e4814e337e23606fc42fe63
RichardEsquivel <rlesquivel@gmail.com>
2019-10-22 16:42:02 -0500

    Testing basic login

--- spotify-song-suggester/src/components/Login.js
+++ spotify-song-suggester/src/components/Login.js
@@ -13,138 +13,147 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
+import oauth from 'axios-oauth-client';
 import axios from "axios";
 
 
 
 
-function validator(props) {
-    axios
-        .post("https:spotify-song-suggester-app.com/login")
-        .then(res => {
-            console.log("login axios response", res)
-            localStorage.setItem("token", res.data.token)
-            props.history.push("/home")
-        })
-        .catch(err => {
-            console.log("login axios error", err)
-        })
-}
+
 
 function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
+	return (
+		<Typography variant="body2" color="textSecondary" align="center">
+			{'Copyright © '}
 
-            <span> Spotify Song Suggester</span>
+			<span> Spotify Song Suggester</span>
 
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
 }
 
 const useStyles = makeStyles(theme => ({
-    '@global': {
-        body: {
-            backgroundColor: theme.palette.common.white,
-        },
-    },
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+	'@global': {
+		body: {
+			backgroundColor: theme.palette.common.white,
+		},
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
 }));
 
 
 export default function SignIn(props) {
-    const classes = useStyles();
+	const classes = useStyles();
 
-    const [creds, setCreds] = useState({
-        username: "",
-        password: ""
-    })
+	const [creds, setCreds] = useState({
+		username: "",
+		password: ""
+	})
 
-    const handleChange = (event) => {
-        setCreds({ ...creds, [event.target.id]: event.target.value })
-    }
+	const handleChange = (event) => {
+		setCreds({ ...creds, [event.target.name]: event.target.value })
+	}
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in
+	const handleSubmit = event => {
+		event.preventDefault();
+		axios
+			.post('https:spotify-song-suggester-app.herokuapp.com/login', `grant_type=password&username=${creds.username}&password=${creds.password}`
+				, {
+					headers: {
+						Authorization: `Basic ${btoa('lambda-client:lambda-secret')}`,
+						'Content-Type': 'application/x-www-form-urlencoded'
+					}
+				})
+			.then(res => {
+				console.log("login axios response", res)
+				localStorage.setItem("token", res.data.access_token)
+				props.history.push("/home")
+			})
+			.catch(err => {
+				console.log("login axios error", err)
+			})
+	}
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign in
         </Typography>
-                <form className={classes.form} noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        onChange={handleChange}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                        onClick={validator}
-                    >
-                        Sign In
+				<form className={classes.form} noValidate>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						autoFocus
+						onChange={handleChange}
+					/>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+						onChange={handleChange}
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						className={classes.submit}
+						onClick={handleSubmit}
+					>
+						Sign In
           </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link to="/signup">
-                                Forgot password?
+					<Grid container>
+						<Grid item xs>
+							<Link to="/signup">
+								Forgot password?
               </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link to="/signup">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-            <Box mt={8}>
-                <Copyright />
-            </Box>
-        </Container>
-    );
+						</Grid>
+						<Grid item>
+							<Link to="/signup">
+								{"Don't have an account? Sign Up"}
+							</Link>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+			<Box mt={8}>
+				<Copyright />
+			</Box>
+		</Container>
+	);
 }
