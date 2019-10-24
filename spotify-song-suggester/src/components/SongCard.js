45f51cf181e2aafb1a4fc84fdb19f53c1db497b4
RichardEsquivel <rlesquivel@gmail.com>
2019-10-24 18:04:25 -0500

    Merging branches

--- spotify-song-suggester/src/components/SongCard.js
+++ spotify-song-suggester/src/components/SongCard.js
@@ -5,9 +5,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
 import axios from 'axios';
 import axiosWithAuth from '../utils/axiosWithAuth';
-
 // import liked from "./liked";
-
 const SongCardMain = styled.main`
     width:100%;
     height:100px;
@@ -36,18 +34,15 @@ const SongCardMain = styled.main`
     }
  
 `;
-
 export default function SongCard(props) {
 	console.log(props.info, "props in song card");
 	const [liked, setLiked] = useState(false);
-
 	const addToLiked = (event) => {
 		event.preventDefault();
 		if (!liked) {
 			axiosWithAuth()
 				.post(
 					`https://spotify-song-suggester-app.herokuapp.com/songs/song`, props.info.id)
-
 				.then(res => {
 					console.log(res, "response from liking a song")
 				})
@@ -56,49 +51,37 @@ export default function SongCard(props) {
 				})
 		}
 		setLiked(true);
-
-				.then(res => {
-			console.log(res, "response from liking a song")
-		})
-		.catch(err => {
-			console.log(err, "error from liking a song")
-		})
-}
-setLiked(true);
-
 	}
-const deleted = (event) => {
-	event.preventDefault();
-	setLiked(false);
-	// axios
-	// .delete()
-	// .then(res =>{
-	//     console.log(res, "response from liking a song")
-	// })
-	// .catch(err => {
-	//     console.log(err, "error from liking a song")
-	// })
-}
-
-if (props.info) {
-	return (
-		<SongCardMain>
-			<div className="FavoriteIconDiv" id={props.info.id} onClick={addToLiked}>
-				<FavoriteBorderIcon className={(!liked) ? "dodisplay" : "dontdisplay"}></FavoriteBorderIcon>
-				<FavoriteIcon className={(liked) ? "dodisplay" : "dontdisplay"}></FavoriteIcon>
-			</div>
-			<div className="info">
-				<iframe className="iframe" src={`https://embed.spotify.com/?uri=${props.info.uri}`} width="400px" height="100px" />
-			</div>
-			<div id={props.info.id} className={(liked) ? "dodisplayflex DeleteIconDiv" : "dontdisplay DeleteIconDiv"} onClick={deleted}>
-				<RemoveCircleOutlineIcon></RemoveCircleOutlineIcon>
-			</div>
-		</SongCardMain>
-	)
-} else if (!props.info) {
-	return (
-		<div>loading page...</div>
-	)
-}
-
+	const deleted = (event) => {
+		event.preventDefault();
+		setLiked(false);
+		// axios
+		// .delete()
+		// .then(res =>{
+		//     console.log(res, "response from liking a song")
+		// })
+		// .catch(err => {
+		//     console.log(err, "error from liking a song")
+		// })
+	}
+	if (props.info) {
+		return (
+			<SongCardMain>
+				<div className="FavoriteIconDiv" id={props.info.id} onClick={addToLiked}>
+					<FavoriteBorderIcon className={(!liked) ? "dodisplay" : "dontdisplay"}></FavoriteBorderIcon>
+					<FavoriteIcon className={(liked) ? "dodisplay" : "dontdisplay"}></FavoriteIcon>
+				</div>
+				<div className="info">
+					<iframe className="iframe" src={`https://embed.spotify.com/?uri=${props.info.uri}`} width="400px" height="100px" />
+				</div>
+				<div id={props.info.id} className={(liked) ? "dodisplayflex DeleteIconDiv" : "dontdisplay DeleteIconDiv"} onClick={deleted}>
+					<RemoveCircleOutlineIcon></RemoveCircleOutlineIcon>
+				</div>
+			</SongCardMain>
+		)
+	} else if (!props.info) {
+		return (
+			<div>loading page...</div>
+		)
+	}
 }
