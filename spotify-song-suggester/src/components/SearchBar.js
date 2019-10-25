2b88e5c3fd8907430a937ff220c84c9970a7c714
RichardEsquivel <rlesquivel@gmail.com>
2019-10-25 11:10:50 -0500

    Merging documents

--- spotify-song-suggester/src/components/SearchBar.js
+++ spotify-song-suggester/src/components/SearchBar.js
@@ -30,71 +30,71 @@ const StyledDiv = styled.div`
 `;
 
 export default function SearchBar(props) {
-    const { setSongs, recommendedIsChecked, setRecommendedIsChecked, setRecs, setMainGraphUrl } = props
+	const { setSongs, recommendedIsChecked, setRecommendedIsChecked, setRecs, setMainGraphUrl } = props
 
-    function delayedRouter() {
-        setTimeout(() => {
-            props.history.push("/search")
-        }, 4000);
-    }
+	function delayedRouter() {
+		setTimeout(() => {
+			props.history.push("/search")
+		}, 4000);
+	}
 
-    const submit = (event) => {
-        event.preventDefault();
-        let value = document.querySelector("#search").value.toLowerCase();
-        if (!recommendedIsChecked) {
-            axios
-                .get(`https://spotify-song-suggester-app.herokuapp.com/data/search/${value}`)
-                .then(res => {
-                    console.log("axios from UNCHECKED searchbar", res)
-                    setSongs(res.data);
-                    delayedRouter();
-                    document.querySelector("#search").value = "";
-                })
-                .catch(err => {
-                    console.log("axios error UNCHECKED searchbar", err)
-                })
-        }
-        else if (recommendedIsChecked) {
-            // both axios are #DS endpoints here
-            axios
-                .get(`https://spotify-api-helper.herokuapp.com/auto_search/DReaI4d55IIaiD6P9/${value}`)
-                .then(res => {
-                    console.log("axios from CHECKED searchbar", res)
-                    setRecs(res.data);
-                    delayedRouter();
-                    document.querySelector("#search").value = "";
-                    axios
-                        .get(`https://spotify-api-helper.herokuapp.com/graph_data/DReaI4d55IIaiD6P9/${res.data[0].id}`)
-                        .then(results => {
-                            console.log(res, "res from Checked search for the 5 way graph display uri")
-                            setMainGraphUrl(results.data[0].graph_uri)
-                        })
-                        .catch(err => {
-                            console.log(err, "err from Checked search for the 5 way graph display uri")
-                        })
-                })
-                .catch(err => {
-                    console.log("axios error CHECKED searchbar", err)
-                })
-        }
+	const submit = (event) => {
+		event.preventDefault();
+		let value = document.querySelector("#search").value.toLowerCase();
+		if (!recommendedIsChecked) {
+			axios
+				.get(`https://spotify-song-suggester-app.herokuapp.com/data/search/`)
+				.then(res => {
+					console.log("axios from UNCHECKED searchbar", res)
+					setSongs(res.data);
+					delayedRouter();
+					document.querySelector("#search").value = "";
+				})
+				.catch(err => {
+					console.log("axios error UNCHECKED searchbar", err)
+				})
+		}
+		else if (recommendedIsChecked) {
+			// both axios are #DS endpoints here
+			axios
+				.get(`https://spotify-api-helper.herokuapp.com/auto_search/DReaI4d55IIaiD6P9/${value}`)
+				.then(res => {
+					console.log("axios from CHECKED searchbar", res)
+					setRecs(res.data);
+					delayedRouter();
+					document.querySelector("#search").value = "";
+					axios
+						.get(`https://spotify-api-helper.herokuapp.com/graph_data/DReaI4d55IIaiD6P9/${res.data[0].id}`)
+						.then(results => {
+							console.log(res, "res from Checked search for the 5 way graph display uri")
+							setMainGraphUrl(results.data[0].graph_uri)
+						})
+						.catch(err => {
+							console.log(err, "err from Checked search for the 5 way graph display uri")
+						})
+				})
+				.catch(err => {
+					console.log("axios error CHECKED searchbar", err)
+				})
+		}
 
-    }
-    const toggleCheckBox = () => {
-        setRecommendedIsChecked(!recommendedIsChecked)
-    }
+	}
+	const toggleCheckBox = () => {
+		setRecommendedIsChecked(!recommendedIsChecked)
+	}
 
-    return (
-        <>
-            <StyledDiv>
-                <SearchIcon className="SearchIcon"></SearchIcon>
-                <input type="text" name="search" id="search" placeholder="Search" autofill="on" />
-                <CheckCircleOutlineIcon className={CheckCircleOutlineIcon} onClick={submit}></CheckCircleOutlineIcon>
-            </StyledDiv>
+	return (
+		<>
+			<StyledDiv>
+				<SearchIcon className="SearchIcon"></SearchIcon>
+				<input type="text" name="search" id="search" placeholder="Search" autofill="on" />
+				<CheckCircleOutlineIcon className={CheckCircleOutlineIcon} onClick={submit}></CheckCircleOutlineIcon>
+			</StyledDiv>
 
-            <CheckBoxDiv>
-                <label htmlFor="checkbox">Search by Related</label>
-                <input type="checkbox" id="checkbox" name="checkbox" value="checkbox" checked={recommendedIsChecked} onClick={toggleCheckBox} />
-            </CheckBoxDiv>
-        </>
-    )
+			<CheckBoxDiv>
+				<label htmlFor="checkbox">Search by Related</label>
+				<input type="checkbox" id="checkbox" name="checkbox" value="checkbox" checked={recommendedIsChecked} onClick={toggleCheckBox} />
+			</CheckBoxDiv>
+		</>
+	)
 }
