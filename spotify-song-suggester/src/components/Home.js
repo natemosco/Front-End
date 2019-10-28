68d1e043b59091703af90655b5a6a2757a1ba7a1
Nathaniel Mosco <natemosco@gmail.com>
2019-10-28 14:57:30 -0400

    near final changes to project

--- spotify-song-suggester/src/components/Home.js
+++ spotify-song-suggester/src/components/Home.js
@@ -1,74 +1,125 @@
 import React, { useState, useEffect } from 'react';
-import SideNav from "./SideNav";
 import axiosWithAuth from '../utils/axiosWithAuth';
 import axios from "axios";
+import HelpIcon from '@material-ui/icons/Help';
+import styled from "styled-components";
+
+
+import SideNav from "./SideNav";
 import SongCard from './SongCard';
 import Modal from "./Modal";
-import HelpIcon from '@material-ui/icons/Help';
 import data from "../data.json";
+import Loading from "./Loading"
+
+
+const StyledMain = styled.main`
+    margin: 100px 0 0 240px;
+    display flex;
+    width:1180;
+    .left{
+        display:flex;
+        flex-wrap:wrap;
+        width:50%; 
+        span{
+            margin: 10% 0 0 1%;
+        }
+        h2{
+            width: 70%;
+            margin-left: 5%;
+        }
+    }
+    .right{
+        width:50%;
+        div{
+            margin-top: -100px;
+        }
+
+    }
+`;
 
 export default function Home(props) {
-	let { setSongs, favoritesIdOnly, recommendedIsChecked, setRecommendedIsChecked, setRecs, setMainGraphUrl } = props
+    let { setUpdatedFavorites, updatedFavorites, setSongs, favoritesIdOnly, recommendedIsChecked, setRecommendedIsChecked, setRecs, setMainGraphUrl } = props
 
-	const [topFive, setTopFive] = useState([]);
-	const [graphUri, setGraphUri] = useState("")
-	const [songCharacteristics, setSongCharacteristics] = useState({})
-	const [visible, setVisible] = useState(false);
+    const [topFive, setTopFive] = useState([]);
+    const [graphUri, setGraphUri] = useState("")
+    const [songCharacteristics, setSongCharacteristics] = useState({})
+    const [visible, setVisible] = useState(false);
 
-	//song characteristics function
-	// takes the values and sorts for the values that are High? Low? spits them out
-	//  to display a brief 1-3 liner of: your average song currently displays en emphasis on ________ tempo? acousticness?
-	// 
+    //song characteristics function
+    // takes the values and sorts for the values that are High? Low? spits them out
+    //  to display a brief 1-3 liner of: your average song currently displays en emphasis on ________ tempo? acousticness?
+    // 
 
-	useEffect(() => {
+    useEffect(() => {
 
-		axios
-			.get(`https://spotify-api-helper.herokuapp.com/playlist_mood_recs/DReaI4d55IIaiD6P9?playlist=${favoritesIdOnly}`)
-			.then(res => {
+        axios
+            // .get(`https://spotify-api-helper.herokuapp.com/playlist_mood_recs/DReaI4d55IIaiD6P9?playlist=${favoritesIdOnly}`)
+            .get(`https://spotify-api-helper.herokuapp.com/playlist_recs/DReaI4d55IIaiD6P9?playlist=${favoritesIdOnly}`)
+            .then(res => {
+                console.log("Home Page response", res);
+                let modifiedObjectShape3 = res.data[2].map(objectTBR => {
+                    objectTBR.trackid = objectTBR.id
+                    objectTBR.imagesongid = 0
+                    delete objectTBR.id
+                    return objectTBR
+                })
+                setTopFive(modifiedObjectShape3);
+                setGraphUri(res.data[0].graph_uri);
+                setSongCharacteristics(res.data[1]);
+            })
+            .catch(err => {
+                console.log(err, "err for myCurrentTopFiveArray on HomePage")
+            })
+    }, [])
 
-				setTopFive(res.data[2]);
-				setGraphUri(res.data[0]);
-				setSongCharacteristics(res.data[1]);
-			})
-			.catch(err => {
-				console.log(err, "err for myCurrentTopFiveArray on HomePage")
-			})
-	}, [favoritesIdOnly])
+    if (topFive.length < 1) {
+        return (
+            <>
+                <SideNav {...props} setSongs={setSongs} recommendedIsChecked={recommendedIsChecked} setRecommendedIsChecked={setRecommendedIsChecked} setRecs={setRecs} setMainGraphUrl={setMainGraphUrl}></SideNav>
+                <br /><br /><br /><br />
+                <div><Loading></Loading>Loading your most current profile changes...</div>
+            </>
+        )
+    } else {
+        const showHideModal = () => {
+            setVisible(!visible);
+        }
+        const helpSpan = document.querySelector(".question-mark")
+        const hoverOn = (event) => {
+            helpSpan.addEventListener("mouseenter", () => {
+                helpSpan.style.transform = "scale(1.2)"
+                helpSpan.style.transition = "all 0.3s"
+            })
+        }
+        const hoverOff = (event) => {
+            helpSpan.addEventListener("mouseleave", () => {
+                helpSpan.style.transform = "scale(1)";
+                helpSpan.style.transition = "all 0.3"
+            })
+        }
 
-	const showHideModal = () => {
-		setVisible(!visible);
-	}
+        return (
+            <>
+                <div>
+                    <SideNav {...props} setSongs={setSongs} recommendedIsChecked={recommendedIsChecked} setRecommendedIsChecked={setRecommendedIsChecked} setRecs={setRecs} setMainGraphUrl={setMainGraphUrl}></SideNav>
+                    <StyledMain>
+                        <section className="left">
+                            <span className="question-mark" onClick={showHideModal} onMouseEnter={hoverOn} onMouseLeave={hoverOff}>
+                                <HelpIcon style={{ height: "24px" }} ></HelpIcon>
 
-	if (!topFive) {
-		return (
-			<>
-				<SideNav {...props} setSongs={setSongs} recommendedIsChecked={recommendedIsChecked} setRecommendedIsChecked={setRecommendedIsChecked} setRecs={setRecs} setMainGraphUrl={setMainGraphUrl}></SideNav>
-				<br /><br /><br /><br />
-				<div>loading...</div>
-			</>
-		)
-	} else {
-		return (
-			<>
-				<div>
-					<SideNav {...props} setSongs={setSongs} recommendedIsChecked={recommendedIsChecked} setRecommendedIsChecked={setRecommendedIsChecked} setRecs={setRecs} setMainGraphUrl={setMainGraphUrl}></SideNav>
-					<main>
-						<section className="left">
-							<h2>Your Current Top 5 Spotify Song Suggestions</h2>
-							<span className="question-mark" onClick={showHideModal}>
-								<HelpIcon style={{ height: "24px" }} ></HelpIcon>
+                            </span>
+                            <h2>Your Current Top 5 Spotify Song Suggestions</h2>
+                            {topFive.map((song, index) => <SongCard setUpdatedFavorites={setUpdatedFavorites} updatedFavorites={updatedFavorites} key={index} info={song} />)}
+                        </section>
+                        <section className="right">
 
-							</span>
-							{topFive.map((song, index) => <SongCard key={index} info={song} />)}
-						</section>
-						<section className="right">
-
-							<embed type="image/svg+xml" src={graphUri} width="600" height="600" />
-						</section>
-					</main>
-				</div>
-				<Modal className={(visible) ? "dodisplayflex" : "dontdisplay"} setVisible={setVisible} header={data.home.header_songs} body={data.home.body_songs}></Modal>
-			</>
-		)
-	}
+                            <div><embed type="image/svg+xml" src={graphUri} width="600" height="600" /></div>
+                        </section>
+                    </StyledMain>
+                </div>
+                <Modal className={(visible) ? "dodisplayflex" : "dontdisplay"} setVisible={setVisible} header={data.home.header_songs} body={data.home.body_songs}></Modal>
+            </>
+        )
+    }
 }
+
--- spotify-song-suggester/src/components/SavedSongs.js
+++ spotify-song-suggester/src/components/SavedSongs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SideNav from "./SideNav";
 import SortField from "./SortField";
 import FavListItem from "./FavListItem";
@@ -10,7 +10,7 @@ const DivLimiter = styled.div`
 
 
 export default function SavedSongs(props) {
-    let { favorites, setSongs, recommendedIsChecked, setRecommendedIsChecked, setRecs, setMainGraphUrl } = props
+    let { setUpdatedFavorites, updatedFavorites, favorites, setSongs, recommendedIsChecked, setRecommendedIsChecked, setRecs, setMainGraphUrl } = props
     const [sortFields, setSortFields] = useState({
         title: true,
         artist: false,
@@ -32,37 +32,40 @@ export default function SavedSongs(props) {
 
     let favSongArray = [];
 
+
     if (sortFields.title) {
         let sortedTitleArray = [];
         sortedTitleArray = favorites.sort((a, b) => (a.song_name > b.song_name) ? 1 : -1).map((fav, index) => {
-            return <FavListItem key={index} {...props} fav={fav} setRecommendedIsChecked={setRecommendedIsChecked} setMainGraphUrl={setMainGraphUrl} setRecs={setRecs} />
+            return <FavListItem key={index} {...props} fav={fav} setUpdatedFavorites={setUpdatedFavorites} updatedFavorites={updatedFavorites} setRecommendedIsChecked={setRecommendedIsChecked} setMainGraphUrl={setMainGraphUrl} setRecs={setRecs} />
         })
         favSongArray = sortedTitleArray;
     }
     else if (sortFields.artist) {
         let sortedArtistArray = [];
         sortedArtistArray = favorites.sort((a, b) => (a.artist > b.artist) ? 1 : -1).map((fav, index) => {
-            return <FavListItem key={index} {...props} fav={fav} setRecommendedIsChecked={setRecommendedIsChecked} setMainGraphUrl={setMainGraphUrl} setRecs={setRecs} />
+            return <FavListItem key={index} {...props} fav={fav} setUpdatedFavorites={setUpdatedFavorites} updatedFavorites={updatedFavorites} setRecommendedIsChecked={setRecommendedIsChecked} setMainGraphUrl={setMainGraphUrl} setRecs={setRecs} />
         })
         favSongArray = sortedArtistArray;
     }
-    else if (sortFields.duration) {
-        let sortedDurationArray = [];
-        sortedDurationArray = favorites.sort((a, b) => (a.artist > b.artist) ? 1 : -1).map((fav, index) => {
-            return <FavListItem key={index} {...props} fav={fav} setRecommendedIsChecked={setRecommendedIsChecked} setMainGraphUrl={setMainGraphUrl} setRecs={setRecs} />
-        })
-        favSongArray = sortedDurationArray;
-    }
-    else if (sortFields.tempo) {
-        let sortedTempoArray = [];
-        sortedTempoArray = favorites.sort((a, b) => (a.artist > b.artist) ? 1 : -1).map((fav, index) => {
-            return <FavListItem key={index} {...props} fav={fav} setRecommendedIsChecked={setRecommendedIsChecked} setMainGraphUrl={setMainGraphUrl} setRecs={setRecs} />
-        })
-        favSongArray = sortedTempoArray;
-    }
+    // else if (sortFields.duration) {
+    //     let sortedDurationArray = [];
+    //     sortedDurationArray = favorites.sort((a, b) => (a.artist > b.artist) ? 1 : -1).map((fav, index) => {
+    //         return <FavListItem key={index} {...props} fav={fav} setUpdatedFavorites={setUpdatedFavorites} updatedFavorites={updatedFavorites} setRecommendedIsChecked={setRecommendedIsChecked} setMainGraphUrl={setMainGraphUrl} setRecs={setRecs} />
+    //     })
+    //     favSongArray = sortedDurationArray;
+    // }
+    // else if (sortFields.tempo) {
+    //     let sortedTempoArray = [];
+    //     sortedTempoArray = favorites.sort((a, b) => (a.artist > b.artist) ? 1 : -1).map((fav, index) => {
+    //         return <FavListItem key={index} {...props} fav={fav} setUpdatedFavorites={setUpdatedFavorites} updatedFavorites={updatedFavorites} setRecommendedIsChecked={setRecommendedIsChecked} setMainGraphUrl={setMainGraphUrl} setRecs={setRecs} />
+    //     })
+    //     favSongArray = sortedTempoArray;
+    // }
+
+
 
     return (
-        <DivLimiter>
+        <DivLimiter onLoad={window.location.reload}>
             <SideNav {...props} setSongs={setSongs} recommendedIsChecked={recommendedIsChecked} setRecommendedIsChecked={setRecommendedIsChecked} setRecs={setRecs} setMainGraphUrl={setMainGraphUrl}></SideNav>
             <SortField filterSelect={filterSelect}></SortField>
             {favSongArray}
